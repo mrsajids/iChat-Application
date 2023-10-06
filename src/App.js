38d9afb6537afa606f24439e8ce0f586d1d855ae
Sajeed Syed <123931227+mrsajids@users.noreply.github.com>
2023-10-06 22:25:32 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { Routes, Route } from "react-router-dom"
 import Nav from "./components/Nav";
 import Chat from "./components/Chat";
 import Join from "./components/Join";
+import { useState } from "react";
 
 function App() {
+  
   return (
     <div className="App">
      <Nav/>
      <Routes>
-        <Route path="/" element={ <Join/> } />
-        <Route path="chat" element={ <Chat/> } />
-        <Route path="join" element={ <Join/> } />
+        <Route path="/" element={ <Join /> } />
+        <Route path="chat" element={ <Chat /> } />
+        <Route path="join" element={  <Join /> } />
       </Routes>
     </div>
   );
