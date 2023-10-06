0916e52858295d5c761c3f05a371e0afe028773d
Sajeed Syed <123931227+mrsajids@users.noreply.github.com>
2023-10-06 22:27:19 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -3,14 +3,24 @@ const app=express()
 const socketio=require('socket.io')
 const http=require('http')
 const router=require('./router')
-
+const cors=require('cors')
+app.use(cors())
 const PORT = process.env.PORT || 5000;
 
 const server=http.createServer(app)
 const io=socketio(server)
 
+io.on('connection',(socket)=>{
+    console.log("We have a new connection!");
+    
+    socket.on('disconnect',()=>{
+        console.log("User had left!!");
+    })
+})
+
 //router
 app.use(router)
+
 
 
 server.listen(PORT,()=>console.log('server running on ',PORT))
